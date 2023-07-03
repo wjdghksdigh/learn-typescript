d136f64ea7597c7306207ea7ec0a6ebca111570b
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-07-03 20:10:52 +0900

    feat : app.js 수정, 타입스크립트 라이브러리 설치

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,41 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @property {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * 
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+console.log(fetchUser());
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
+      // console.log(user);
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user[1].name;
+      email.innerText = user[1].email;
+      address.innerText = user[1].address.street;
     })
     .catch(function (error) {
       console.log(error);
